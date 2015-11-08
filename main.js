a97e2d7d5dad3daef5cc0b2445cc943ee19bd25a
ChenMeihao <mc5283@nyu.edu>
2015-11-07 23:05:46 -0500

    add word cloud

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(init);
+
+function init() {
+	//drawCloud(d3.select('#word-cloud'));
+	drawCloud(d3.select('#word-cloud'));
+
+	$('.btn').click(function() {
+		$('.btn').removeClass('active');
+		$(this).addClass('active');
+	});
+}
