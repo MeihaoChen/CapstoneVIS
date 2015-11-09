022b1b546bd8210adf4769d96d7f80f372c6db15
ChenMeihao <mc5283@nyu.edu>
2015-11-09 15:44:04 -0500

    add hier graph

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(init);
 
 function init() {
 	drawCloud(d3.select('#word-cloud'));
+	drawHier(d3.select('#hier'));
 
 	$('.btn').click(function() {
 		$('.btn').removeClass('active');
