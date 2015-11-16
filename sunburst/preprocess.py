56bcd04c3f3488fa3d02de1d547bfbb6520d3279
yw652 <yw652@nyu.edu>
2015-11-15 20:28:56 -0500

    finish sunburst revision

--- sunburst/preprocess.py
+++ sunburst/preprocess.py
@@ -51,11 +51,18 @@ def buildData(text):
     
     # Slice more than 6 elements in a sequence will return error
     # still trying to fix that
-	preliminary_final = ['-'.join(i[:6]) for i in preliminary]
+
+	preliminary_final = []
+	for i in preliminary:
+		element = '-'.join(i[:min(len(i)-1,10)])
+		if len(i) < 10:
+			element = element + str('-end')
+		preliminary_final.append(element)
 
 	with open('tweet_text.csv','wb') as f:
 		for key,value in collections.Counter(preliminary_final).iteritems():
-			f.write('%s,%d\n' % (key.encode('ascii', 'ignore'),value))
+			if value > 10:
+                		f.write('%s,%d\n' % (key.encode('ascii', 'ignore'),value))
 	return all_words
 
 if __name__ == '__main__':
@@ -67,7 +74,7 @@ if __name__ == '__main__':
 
 	all_words = buildData(text)
 
-	# Build color match for each word in the data
+	# Build color match for each word in the dat
 	color_profile = {}
 	color = ['3182bd','6baed6','9ecae1','c6dbef','e6550d','fd8d3c','fdae6b','fdd0a2','31a354','74c476','a1d99b',
 			'c7e9c0','756bb1','9e9ac8','bcbddc','dadaeb','636363','969696','bdbdbd','d9d9d9']
